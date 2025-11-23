da3b924430b82f0e328d858cc66d1873a6434c10
Pragathi Kalidasan Vetrivel Murugan <pragathivetrivelmurugan@gmail.com>
2025-11-24 00:14:06 +0530

    Use compatible PyTorch versions (2.0.1 and 0.15.2)

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 # Install regular dependencies first
 pip install -r requirements.txt
 # Install CPU-only PyTorch last (from PyTorch index)
-pip install torch==2.1.0 torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cpu
+pip install torch torchvision
